3d39dae6ec99fe6c3d330f22878807fd25a30f5b
M8MBA <sauce.ix@gmail.com>
2022-02-27 16:32:46 -0700

    README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# Work Day Scheduler
+
+<img width="1395" alt="Screen Shot 2022-02-27 at 4 19 54 PM" src="https://user-images.githubusercontent.com/97080366/155904514-ead02c99-a92c-4c56-b7ce-9fb5fc6ba7e2.png">
+
+## Purpose
+
+The purpose of this project was to build an app that allows the user keep track of their daily tasks. 
+The user is able to input a task into the hourly block and save the task to the local storage so when the
+user refreshes the page they do not lose thier input.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+* jQuery
+
+
+## Website
+
+## Contribution
+Made with ❤️ by Cade Wilson
+
+### ©️2022 Cade Wilson
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,9 @@
 var dateTime = function () {
   var currentDay = document.getElementById("currentDay");
   var time = moment();
-
     currentDay.textContent = time.format("dddd, MMMM Do YYYY, h:mm:ss a");
-
     setInterval(function () { dateTime(); }, 1000);
-
-}
+};
 
 dateTime();
 
@@ -37,13 +34,13 @@ function clickHandler() {
 $(".saveBtn").click(clickHandler)
 
 // retrieve & display text
-$("#9AM").val(localStorage.getItem("9AM"))
-$("#10AM").val(localStorage.getItem("10AM"))
-$("#11AM").val(localStorage.getItem("11AM"))
-$("#12PM").val(localStorage.getItem("12PM"))
-$("#1PM").val(localStorage.getItem("1PM"))
-$("#2PM").val(localStorage.getItem("2PM"))
-$("#3PM").val(localStorage.getItem("3PM"))
-$("#4PM").val(localStorage.getItem("4PM"))
-$("#5PM").val(localStorage.getItem("5PM"))
+$("#9AM").val(localStorage.getItem("9AM"));
+$("#10AM").val(localStorage.getItem("10AM"));
+$("#11AM").val(localStorage.getItem("11AM"));
+$("#12PM").val(localStorage.getItem("12PM"));
+$("#1PM").val(localStorage.getItem("1PM"));
+$("#2PM").val(localStorage.getItem("2PM"));
+$("#3PM").val(localStorage.getItem("3PM"));
+$("#4PM").val(localStorage.getItem("4PM"));
+$("#5PM").val(localStorage.getItem("5PM"));
 
